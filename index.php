e1ca0404b40f72dda9ca08984eba755d21aef1bb
Mteehub <69678080+Mteehub@users.noreply.github.com>
2022-01-07 16:31:53 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php include("HF/header.php") ?>
+
+
+<section class="section-full-img">
+
+<div>
+
+<div>
+    <h1>Portfolio</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, recusandae?</p>
+    <button><a href="#" class="btn">CONTACT</a></button>
+</div>
+
+</div>
+
+</section>
+
+
+
+
+
+
+<div class="contain-card-section">
+<div class="container">
+      <h2>Réalisations</h2>
+      <hr>
+      <div class="card-container">
+
+        <div class="card-wrapper">
+          <div class="card">
+            <div class="card-front front-img-1">
+              <p>Vous résidez à moins d'1km d'un parking ?</p>
+              <p class="card-price">
+                Wordpress
+              </p>
+              <i class="arrow-icon fas fa-arrow-right"></i>
+            </div>
+            <div class="card-back">
+              <a href="#" class="btn">Plus d'informations</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-wrapper">
+          <div class="card">
+            <div class="card-front front-img-2">
+              <p>Vous résidez à moins d'1km d'un parking ?</p>
+              <p class="card-price">
+                64 € / mois
+              </p>
+              <i class="arrow-icon fas fa-arrow-right"></i>
+            </div>
+            <div class="card-back">
+              <a href="#" class="btn">Je prends rendez-vous</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-wrapper">
+          <div class="card">
+            <div class="card-front front-img-3">
+              <p>Vous résidez à moins d'1km d'un parking ?</p>
+              <p class="card-price">
+                64 € / mois
+              </p>
+              <i class="arrow-icon fas fa-arrow-right"></i>
+            </div>
+            <div class="card-back">
+              <a href="#" class="btn">Je prends rendez-vous</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+<div style="display:flex;justify-content:center;">
+      <a href="#" class="btn">Voire toutes les réalisations</a>
+</div>
+
+    </div>
+
+</div>
+
+<?php include("HF/footer.php") ?>
